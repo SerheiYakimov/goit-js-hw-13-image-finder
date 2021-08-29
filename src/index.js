9df277aa216503f405b800bd1269c48ec30fb70a
SerheiYakimov <ysergey2901@gmail.com>
2021-08-30 01:29:33 +0300

    добавил слушатель на input и стили картинкам

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-import searchFormTps from './templates/search-form-tps.hbs';
+
 import galleryCardTps from './templates/gallery-card-tps.hbs';
+
 
 import './sass/main.scss';
 
 
-fetch('https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=23141272-55f7853bfecadbbcd9800c5ad')
+
+
+const refs = {
+    form: document.querySelector('#search-form'),
+    input: document.querySelector('input')
+   
+}
+
+refs.input.addEventListener('input', onFetchImages);
+
+function onFetchImages(e) {
+e.preventDefault();
+const value = refs.input.value;
+console.log(value);
+}
+
+
+
+// refs.form.insertAdjacentHTML('afterbegin', galleryCardTps);
+
+
+// fetch('https://pixabay.com/api/?image_type=photo&orientation=horizontal&key=23141272-55f7853bfecadbbcd9800c5ad')
+// .then(response => response.json())
+// .then(result => console.log(result))
+
+// fetch('https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=23141272-55f7853bfecadbbcd9800c5ad')
+// .then(response => response.json())
+// .then(result => console.log(result))
