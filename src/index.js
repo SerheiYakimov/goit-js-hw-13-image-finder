f57ad376a76841e93c9c9a3e3a448dd117989b89
SerheiYakimov <ysergey2901@gmail.com>
2021-08-29 23:28:49 +0300

    добавил hbs tps

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import searchFormTps from './templates/search-form-tps.hbs';
+import galleryCardTps from './templates/gallery-card-tps.hbs';
+
 import './sass/main.scss';
+
+
+fetch('https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=23141272-55f7853bfecadbbcd9800c5ad')
