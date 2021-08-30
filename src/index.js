66ec474ac7b9f1900f1339dfcc0c9aacbde4142e
SerheiYakimov <ysergey2901@gmail.com>
2021-08-31 00:23:01 +0300

    добавил
    1. debounce
    2. функцию очистки инпута при наборе

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 
 import galleryCardTps from './templates/gallery-card-tps.hbs';
-
-
+import debounce from 'lodash.debounce';
 import './sass/main.scss';
 
 
@@ -10,40 +9,52 @@ import './sass/main.scss';
 const refs = {
     form: document.querySelector('#search-form'),
     input: document.querySelector('input'),
-    more: document.querySelector('#more'),
+    moreBtn: document.querySelector('#more'),
     gallery: document.querySelector('.gallery')
    
 }
 
-refs.input.addEventListener('input', onFetchImages);
-refs.more.addEventListener('click', onFetchImages)
-
-function onFetchImages(e) {
-e.preventDefault();
-const value = refs.input.value;
-console.log(value);
-
-return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${value}&key=23141272-55f7853bfecadbbcd9800c5ad`)
-.then(response => response.json())
-// .then(renderImages)
-.then(result => renderImages(result.hits))
-}
-
-
-function renderImages() {
-    const markUpImages = galleryCardTps()
-    refs.gallery.insertAdjacentHTML('afterend', markUpImages);
-}
+refs.input.addEventListener('input', debounce(onFetchImages), 1000);
+// refs.moreBtn.addEventListener('click', onFetchImages);
 
 let currentPage = 1;
 
 
+function onFetchImages(e) {
+e.preventDefault();
+const value = e.target.value;
+console.log(value);
 
-
-
-
-// fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${value}&page=${currentPage}&per_page=12&key=23141272-55f7853bfecadbbcd9800c5ad`)
+// return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${value}&key=23141272-55f7853bfecadbbcd9800c5ad`)
 // .then(response => response.json())
+// .then(result => renderImages(result.hits))
+
+
+return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${value}&page=${currentPage}&per_page=12&key=23141272-55f7853bfecadbbcd9800c5ad`)
+.then(response => response.json())
 // .then(result => console.log(result))
-// .then(() => currentPage++)
-// .catch((err) => console.log(error))
+.then(result => {
+    clearGallery();
+    renderImages(result.hits);    
+})    
+.then(() => currentPage++)
+.catch((err) => console.log(error))
+}
+
+
+function renderImages(hits) {
+    const markUpImages = galleryCardTps(hits);
+    refs.gallery.insertAdjacentHTML('afterbegin', markUpImages);
+}
+
+
+function clearGallery() {
+    refs.gallery.innerHTML = '';
+}
+
+
+
+
+
+
+
