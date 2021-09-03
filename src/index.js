c07c774c489f822504789214a40523fb47834b85
SerheiYakimov <ysergey2901@gmail.com>
2021-09-04 02:49:44 +0300

    добавил кнопку up и функционал к ней

--- src/index.js
+++ src/index.js
@@ -9,32 +9,44 @@ const refs = {
     form: document.querySelector('#search-form'),
     input: document.querySelector('input'),
     moreBtn: document.querySelector('#more'),
-    gallery: document.querySelector('.gallery')   
+    gallery: document.querySelector('.gallery'),
+    upBtn: document.querySelector('#up')      
 }
 
 refs.form.addEventListener('submit', onFetchImages);
 refs.moreBtn.addEventListener('click', onLoadMore);
+refs.upBtn.addEventListener('click', onScrollUp);
+
 
 
 const imageApiService = new ImageApiService();
+const hiddenElement = document.getElementById('more');
+
+
 
 
 function onFetchImages(e) {
     e.preventDefault();
     clearGallery();
-   
+    
     
     imageApiService.value = e.currentTarget.elements.query.value;
     console.log(imageApiService.value);
     imageApiService.resetPage();
     imageApiService.fetchGallery().then(renderImages);
+   
     refs.moreBtn.classList.replace('btn-hidden', 'btn-open');
+    refs.upBtn.classList.replace('btn-hidden', 'btn-open');
+    
 }
 
 
 function renderImages(hits) {
     const markUpImages = galleryCardTps(hits);
     refs.gallery.insertAdjacentHTML('beforeend', markUpImages);
+    showScroll();
+    
+    
 }
 
 
@@ -44,13 +56,28 @@ function clearGallery() {
 
 function onLoadMore() {
     imageApiService.fetchGallery().then(renderImages);
+   
+   
+    
 }
 
-// const element = document.getElementById('.my-element-selector');
-// element.scrollIntoView({
-//   behavior: 'smooth',
-//   block: 'end',
-// });
+
+function showScroll() {
+    hiddenElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end'
+      });
+}
+
+function onScrollUp() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+
+
 
 
 
