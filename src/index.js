e9e0044161539a7495e3a54ed38ec1a7d5beb502
SerheiYakimov <ysergey2901@gmail.com>
2021-09-05 14:18:04 +0300

    добавил функцию onShowFullImage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 
 import ImageApiService from './js/apiService';
 import galleryCardTps from './templates/gallery-card-tps.hbs';
+// import modalImage from './templates/modal-image.hbs';
 import { alert } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
 import * as basicLightbox from 'basiclightbox';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css'
 import './sass/main.scss';
 
 
@@ -13,7 +15,8 @@ const refs = {
     input: document.querySelector('input'),
     moreBtn: document.querySelector('#more'),
     gallery: document.querySelector('.gallery'),
-    upBtn: document.querySelector('#up')      
+    upBtn: document.querySelector('#up')
+    
 }
 
 refs.form.addEventListener('submit', onFetchImages);
@@ -21,7 +24,7 @@ refs.moreBtn.addEventListener('click', onLoadMore);
 refs.upBtn.addEventListener('click', onScrollUp);
 
 
-
+   
 const imageApiService = new ImageApiService();
 const hiddenElement = document.getElementById('more');
 
@@ -44,7 +47,9 @@ function onFetchImages(e) {
     else {
     imageApiService.resetPage();
     imageApiService.fetchGallery().then(renderImages);
-   
+        
+    refs.gallery.addEventListener('click', onShowFullImage);
+    
     refs.moreBtn.classList.replace('btn-hidden', 'btn-open');
     refs.upBtn.classList.replace('btn-hidden', 'btn-open');
     }
@@ -61,6 +66,12 @@ function renderImages(hits) {
     
 }
 
+// function renderLargeImage(hits) {
+//     const markUpLargeImage = modalImage(hits);
+//     refs.gallery.insertAdjacentHTML('beforeend', markUpLargeImage);
+
+// }
+
 
 function clearGallery() {
     refs.gallery.innerHTML = '';
@@ -69,9 +80,7 @@ function clearGallery() {
 
 function onLoadMore() {
     imageApiService.fetchGallery().then(renderImages);
-   
-   
-    
+          
 }
 
 
@@ -91,16 +100,21 @@ function onScrollUp() {
 }
 
 
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-`)
 
-function showFullImage() {
+
+// instance.show()
+
+function onShowFullImage(e) {
+    e.preventDefault();
+    console.log(e.target.nodeName);
+    
+
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+    const src = imageApiService.fetchGallery().then(this.largeImageURL);
+
+    const instance = basicLightbox.create(`<img src=${src} width="1280" height="600" />`);
     instance.show()
 }
 
